4d7a7d39c8da8691da04d95abc61b49e16ba70aa
stardust <876700669@qq.com>
2024-07-26 16:31:32 +0800

    add:公共组件title

--- components/common-title/common-title.vue
+++ components/common-title/common-title.vue
@@ -1,7 +1,21 @@
 <template>
-	<view>公共标题</view>
+	<view class="common-title">
+		<view class="name">
+			<slot name="name"></slot>
+		</view>
+		<view class="custom">
+			<slot name="custom"></slot>
+		</view>
+	</view>
 </template>
 
 <script setup></script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.common-title {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0 30rpx;
+}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -21,7 +21,17 @@
 		</view>
 
 		<view class="select">
-			<common-title></common-title>
+			<common-title>
+				<template #name>每日推荐</template>
+				<template #custom>
+					<view class="date">
+						<uni-icons type="calendar" size="16" color="#28b389"></uni-icons>
+						<view class="text">
+							<uni-dateformat :date="Date.now()" format="dd日"></uni-dateformat>
+						</view>
+					</view>
+				</template>
+			</common-title>
 			<view class="content">
 				<scroll-view scroll-x>
 					<view class="box" v-for="item in 8">
@@ -29,6 +39,15 @@
 					</view>
 				</scroll-view>
 			</view>
+		</view>
+
+		<view class="theme">
+			<common-title>
+				<template #name>专题精选</template>
+				<template #custom>
+					<navigator url="" class="more">More+</navigator>
+				</template>
+			</common-title>
 		</view>
 	</view>
 </template>
@@ -100,6 +119,14 @@
 
 	.select {
 		padding-top: 50rpx;
+		.date {
+			color: #28b389;
+			display: flex;
+			align-items: center;
+			.text {
+				margin-left: 5rpx;
+			}
+		}
 		.content {
 			width: 720rpx;
 			margin-left: 30rpx;
@@ -124,6 +151,14 @@
 					margin-right: 30rpx;
 				}
 			}
+		}
+	}
+
+	.theme {
+		padding-top: 50rpx;
+		.more {
+			color: #888;
+			font-size: 32rpx;
 		}
 	}
 }
