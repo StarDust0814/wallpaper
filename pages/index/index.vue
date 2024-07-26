2cf6cc87d7e5209ef50bc7fa2212c8e651a12898
stardust <876700669@qq.com>
2024-07-26 15:47:26 +0800

    add:添加首页轮播图

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,37 @@
 <template>
-	<view class="homeLayout"></view>
+	<view class="homeLayout">
+		<view class="banner">
+			<swiper circular indicator-dots indicator-active-color="#fff" indicator-color="rgba(255,255,255,0.5)" autoplay>
+				<swiper-item v-for="item in 3">
+					<image src="../../common/images/banner1.jpg" mode="aspectFill"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+	</view>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.homeLayout {
+	.banner {
+		width: 750rpx;
+		padding: 30rpx 0;
+		swiper {
+			width: 750rpx;
+			height: 340rpx;
+			/* &代表将父级名称填到此处 */
+			&-item {
+				width: 100%;
+				height: 100%;
+				padding: 0 30rpx;
+				image {
+					width: 100%;
+					height: 100%;
+					border-radius: 10rpx;
+				}
+			}
+		}
+	}
+}
+</style>
