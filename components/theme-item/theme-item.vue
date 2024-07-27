8d3b28b71ab6d7a3793860c867bf825d0fcd9482
stardust <876700669@qq.com>
2024-07-27 10:44:15 +0800

    add:props传递参数判断专题卡片渲染更多

--- components/theme-item/theme-item.vue
+++ components/theme-item/theme-item.vue
@@ -1,14 +1,29 @@
 <template>
 	<view class="themeItem">
-		<navigator url="" class="box">
+		<navigator url="" class="box" v-if="!isMore">
 			<image class="pic" src="../../common/images/classify1.jpg" mode="aspectFill"></image>
 			<view class="mask">明星美女</view>
 			<view class="tab">3天前更新</view>
 		</navigator>
+		<navigator url="" class="box more" v-if="isMore">
+			<image class="pic" src="../../common/images/more.jpg" mode="aspectFill"></image>
+			<view class="mask">
+				<uni-icons type="more-filled" size="34" color="#fff"></uni-icons>
+				<view class="text">更多</view>
+			</view>
+		</navigator>
 	</view>
 </template>
 
-<script setup></script>
+<script setup>
+// 判断网格中要渲染的是more选项卡还是正常的图片
+defineProps({
+	isMore: {
+		type: Boolean,
+		default: false
+	}
+});
+</script>
 
 <style lang="scss">
 .themeItem {
@@ -49,6 +64,16 @@
 			font-size: 22rpx;
 			transform: scale(0.8);
 			transform-origin: left top;
+		}
+	}
+	.box.more {
+		.mask {
+			width: 100%;
+			height: 100%;
+			flex-direction: column;
+		}
+		.text {
+			font-size: 28rpx;
 		}
 	}
 }
