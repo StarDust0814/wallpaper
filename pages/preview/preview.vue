81ea05626b2421ca80205b7e3d8f576b4e731419
stardust <876700669@qq.com>
2024-07-28 11:23:40 +0800

    add:评分弹窗布局

--- pages/preview/preview.vue
+++ pages/preview/preview.vue
@@ -19,7 +19,7 @@
 					<uni-icons type="info" size="28"></uni-icons>
 					<view class="text">信息</view>
 				</view>
-				<view class="box">
+				<view class="box" @click="clickScore">
 					<uni-icons type="star" size="28"></uni-icons>
 					<view class="text">5分</view>
 				</view>
@@ -76,23 +76,58 @@
 				</scroll-view>
 			</view>
 		</uni-popup>
+
+		<uni-popup ref="scorePopup" :is-mask-click="false">
+			<view class="scorePopup">
+				<view class="popHeader">
+					<view></view>
+					<view class="title">壁纸评分</view>
+					<view class="close" @click="clickScoreClose">
+						<uni-icons type="closeempty" size="18" color="#999"></uni-icons>
+					</view>
+				</view>
+				<view class="content">
+					<uni-rate v-model="userScore" allowHalf></uni-rate>
+					<text class="text">{{ userScore }}分</text>
+				</view>
+
+				<view class="footer">
+					<button @click="submitScore" :disabled="!userScore" type="default" size="mini" plain>确认评分</button>
+				</view>
+			</view>
+		</uni-popup>
 	</view>
 </template>
 
 <script setup>
 const maskState = ref(true);
 const infoPopup = ref(null);
+const scorePopup = ref(null);
 // 点击弹窗事件
 const clickInfo = () => {
 	infoPopup.value.open();
+};
+const clickScore = () => {
+	scorePopup.value.open();
 };
 // 关闭弹窗事件
 const clickInfoClose = () => {
 	infoPopup.value.close();
 };
+
+const clickScoreClose = () => {
+	scorePopup.value.close();
+};
+
 const maskChange = () => {
 	maskState.value = !maskState.value;
 };
+
+// 分数双向绑定
+const userScore = ref(0);
+// 分数提交
+
+const submitScore = () => {};
 </script>
 
 <style lang="scss" scoped>
@@ -237,6 +272,44 @@ const maskChange = () => {
 					line-height: 1.6em;
 				}
 			}
+		}
+	}
+
+	.scorePopup {
+		background: #fff;
+		padding: 30rpx;
+		width: 70vw;
+		border-radius: 30rpx;
+		.popHeader {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			.title {
+				color: $text-font-color-2;
+				font-size: 26rpx;
+			}
+			.close {
+				padding: 6rpx;
+			}
+		}
+		.content {
+			padding: 30rpx 0;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			.text {
+				color: #ffca3e;
+				padding-left: 10rpx;
+				width: 80rpx;
+				line-height: 1em;
+				text-align: right;
+			}
+		}
+		.footer {
+			padding: 10rpx 0;
+			display: flex;
+			align-items: center;
+			justify-content: center;
 		}
 	}
 }
