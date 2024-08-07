3e53e77754736042e2887e82ff506e8844e1b0ae
stardust <876700669@qq.com>
2024-08-07 21:05:09 +0800

    add:搜索页面

--- components/custom-nav-bar/custom-nav-bar.vue
+++ components/custom-nav-bar/custom-nav-bar.vue
@@ -4,10 +4,10 @@
 			<view class="statusBar" :style="{ height: getStatusBarHeight() + 'px' }"></view>
 			<view class="titleBar" :style="{ height: getTitleBarHeight() + 'px', marginLeft: getLeftIconLeft() + 'px' }">
 				<view class="title">{{ title }}</view>
-				<view class="search">
+				<navigator url="/pages/search/search" class="search">
 					<uni-icons type="search" size="18" color="#888"></uni-icons>
 					<text class="text">搜索</text>
-				</view>
+				</navigator>
 			</view>
 		</view>
 
